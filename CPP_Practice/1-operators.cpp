210d1efe4f453ca6b075af7ac26501af85272038
alanjames00 <alanjames6282@gmail.com>
2023-08-03 17:28:36 +0530

    Add problems for 03/08/23

---
+++ CPP_Practice/1-operators.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int num1,num2;
+  cout<<"Enter number1 and number2: "<<endl;
+  cin>>num1>>num2;
+  
+  cout<<"num2 + num1 = "<<(num1+num2)<<endl;
+  cout<<"num2 * num1 = "<<(num1*num2)<<endl;
+  cout<<"num1 % num2 = "<<(num1%num2)<<endl;
+
+  cout<<"num1 > num2 = "<<(num1%num2 )<<endl;
+  cout<<"num1 <= num2 = "<<(num1<=num2)<<endl;
+
+  
+    
+
+
+
+  return 0;
+}
---
+++ CPP_Practice/2-if_conditionals.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int num;
+  cout<<"Enter a number to check for Odd/Even: "<<endl;
+  cin>>num;
+  if(num%2==0){
+    cout<<"Even";
+  }
+  else{
+    cout<<"Odd";
+  }
+
+
+  return 0;
+}
