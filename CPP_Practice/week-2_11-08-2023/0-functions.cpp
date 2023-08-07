91c25798d060b2b6cbf8b18ce205b7d1badefe1a
alanjames00 <alanjames6282@gmail.com>
2023-08-07 20:42:38 +0530

    complete work for 07-08-2023

---
+++ CPP_Practice/week-2_11-08-2023/0-functions.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+
+//Write the code for a void fn that takes two inputs of different type
+void printMarks(string name, int marks){
+    cout<<"My name is "<<name<<" and i got "<<marks<<" in 12th";
+}
+
+
+
+int main(){
+    printMarks("alan",55);
+
+
+    return 0;
+}
---
+++ CPP_Practice/week-2_11-08-2023/1-functions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//Define a function that return an int by multiplying two given inputs 
+
+
+int mulitply(int x, int y){
+    return x*y;
+}
+
+
+
+int main(){
+
+    cout<<mulitply(7,10);
+    return 0;
+}
