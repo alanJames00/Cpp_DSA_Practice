73b0d7fb01133bf2094ebbf2e57f387da0527d8a
alanjames00 <alanjames6282@gmail.com>
2023-10-16 20:13:46 +0530

    add more self_ref code

--- CPP_Practice/Week7/3_self_referential.cpp
+++ CPP_Practice/Week7/3_self_referential.cpp
@@ -10,11 +10,39 @@ structure of the same type
 A class/struct that points to the class/struct of same type.
 
 e.g in LL, Trees
+
+
+
+
+Self Ref is of two types: single link and multiple link self ref.
 */
 
 // Implementation of self_ref in Class
+// self ref. class with single link
 class Node{
+    public:
     int data;
     Node* next;
 };
 
+
+// Self ref. class with multiple links (Double LL)
+
+
+int main(){
+
+    Node obj1;
+    obj1.next = NULL;
+    obj1.data = 1;
+
+    Node obj2;
+    obj2.next = NULL;
+    obj2.data = 2;
+
+    obj1.next = &obj2;
+
+    cout<<obj1.next<<endl;
+
+
+
+}
